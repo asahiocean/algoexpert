d10b4fc4c89b85f9438e0d9d4b7ab3c850e35c2c
asahiocean <asahiocean@icloud.com>
2021-12-03 12:00:05 +0500

    Validate Subsequence: Refactoring

--- easy/Validate Subsequence/ValidateSubsequence.playground/Resources/Validate Subsequence/ValidateSubsequence.playground/Contents.swift
+++ easy/Validate Subsequence/ValidateSubsequence.playground/Resources/Validate Subsequence/ValidateSubsequence.playground/Contents.swift
@@ -1,11 +1,8 @@
 import Foundation
 
-// Validate Subsequence
 // https://www.algoexpert.io/questions/Validate%20Subsequence
 
-// MARK: - Solution 1 -
-
-class Program {
+class Program { // Solution 1
     static func isValidSubsequence(_ array: [Int], _ sequence: [Int]) -> Bool {
         var j = 0
         for i in array.indices {
@@ -16,9 +13,7 @@ class Program {
     }
 }
 
-// MARK: - Solution 2 -
-
-class Program2 {
+class Program2 { // Solution 2
     static func isValidSubsequence(_ array: [Int], _ sequence: [Int]) -> Bool {
         var i = 0, n = 0
         while n < array.endIndex, i < sequence.endIndex {
@@ -29,9 +24,7 @@ class Program2 {
     }
 }
 
-// MARK: - Solution 3 -
-
-class Program3 {
+class Program3 { // Solution 3
     static func isValidSubsequence(_ array: [Int], _ sequence: [Int]) -> Bool {
         var arrIdx = 0, seqIdx = 0
         while arrIdx < array.count, seqIdx < sequence.count {
@@ -44,26 +37,20 @@ class Program3 {
     }
 }
 
-// MARK: - Test Cases -
-
-// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.006 (0.089) seconds
-
 import XCTest
 
+// Executed 3 tests, with 0 failures (0 unexpected) in 0.006 (0.089) seconds
+
 class Tests: XCTestCase {
-    
     let array = [5, 1, 22, 25, 6, -1, 8, 10]
     let sequence = [1, 6, -1, 10]
-    
-    func test0() {
+    func test1() {
         XCTAssert(Program.isValidSubsequence(array, sequence))
     }
-    
-    func test1() {
+    func test2() {
         XCTAssert(Program2.isValidSubsequence(array, sequence))
     }
-    
-    func test2() {
+    func test3() {
         XCTAssert(Program3.isValidSubsequence(array, sequence))
     }
 }
